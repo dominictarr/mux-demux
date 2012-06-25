e899efb12a7d85a350cdef8db1ff87c62eb97244
Dominic Tarr <dominic.tarr@gmail.com>
2012-06-26 07:33:47 +1200

    update test

--- test/index.js
+++ test/index.js
@@ -9,6 +9,7 @@ var connected, closed, ended, _hi
 
 B.on('connection', function (hi) {
   _hi = hi
+  console.log('CONNECTION')
   a.equal(hi.meta.name, 'hello')
   connected = true
   hi.once('data', function (data) {
@@ -16,23 +17,32 @@ B.on('connection', function (hi) {
   })
 })
 
-A.pipe(es.log('A')).pipe(B)
+A.pipe(B).pipe(A)
 
 var hi = A.createStream({name: 'hello'})
 
 hi.write('whatever')
+
 a.ok(connected)
 
-hi.on('end', function () {
-  ended = true
-})
-
-hi.on('close', function () {
+hi.on('close', function eee () {
+  console.log('CLISED!!!')
   closed = true
 })
 
 hi.end()
+a.equal(hi.writable, false)
+a.equal(_hi.readable, false)
+
 _hi.end()
+a.equal(_hi.writable, false)
+a.equal(hi.readable, false)
+
+hi.on('close', function () { console.log('HI CLOSE') })
+_hi.on('close', function () { console.log('_HI CLOSE') })
+
+console.log('BIG END')
+
 A.end()
 
 a.ok(closed)

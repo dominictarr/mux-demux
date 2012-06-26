713a24715aa8a7688f52f54124000f9534e0dd73
Dominic Tarr <dominic.tarr@gmail.com>
2012-06-26 23:03:29 +1200

    update example now that buffering is removed

--- examples/tcp.js
+++ examples/tcp.js
@@ -4,23 +4,22 @@ var net = require('net')
 var mdm1 = MuxDemux()
 var mdm2 = MuxDemux()
 
-var ds = mdm1.createWriteStream('times')
-
 mdm2.on('connection', function (stream) {
   stream.on('data', function (date) {
     console.log(date)
   })
 })
 
-setInterval(function () {
-  ds.write(new Date().toString())
-}, 1e3)
-
 net.createServer(function (con) {
-  con.pipe(mdm2.getMuxDemuxStream()).pipe(con)
+  con.pipe(mdm2).pipe(con)
 }).listen(8642, function () {
   var con = net.connect(8642)
-  con.pipe(mdm1.getMuxDemuxStream()).pipe(con)
+  con.pipe(mdm1).pipe(con)
+  var ds = mdm1.createWriteStream('times')
+
+  setInterval(function () {
+    ds.write(new Date().toString())
+  }, 1e3)
 })
 
 
